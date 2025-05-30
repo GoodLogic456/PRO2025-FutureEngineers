77fdfe64809d1de7416c8c8b578677facb289fe9
GoodLogic456 <goodlogic456@gmail.com>
2025-05-30 17:09:20 +0800

    1

--- cameraMain1.py
+++ cameraMain1.py
@@ -3,7 +3,7 @@ from serialtalk.auto import SerialTalk
 st = SerialTalk()
 
 
-#gnd,vcc 123456
+#gnd,vcc 123456789
 # , ,rx,tx
 #import sensor,image,time
 #threshHold(L min,L max,A min,A max,B min,B max)
