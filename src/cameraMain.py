7d61b759a93d04f84bc84d65fcc7cfc9414ebc3a
GoodLogic456 <goodlogic456@gmail.com>
2025-07-16 21:39:47 +0800

    update cameraMain.py

--- src/cameraMain.py
+++ src/cameraMain.py
@@ -1,169 +1,124 @@
 import sensor,time,pyb,math
 from serialtalk.auto import SerialTalk
-st = SerialTalk()
-
-
-#gnd,vcc 123456789
-# , ,rx,tx
-#import sensor,image,time
-#threshHold(L min,L max,A min,A max,B min,B max)
-threshHold_red    =(20,100,  15,127,  15,127)
-threshHold_green  =( 0,100,-128,-34,-128,127)
-threshHold_blue   =(25, 46,-128, 21,-128,-42)
-threshHold_orange =(30, 80,  25,127,  25,127)
-threshHold_magenta=(30,100,  50, 90, -15, 20)
-#width,hight
-robotSize=(100,50)
-# z, x, y
-blockSizes=[
-    (10,10,10),#red and green 10cm in actual equal to screen 0.058cm
-    (200,20,100)#magenta
-]
-#robot angle=use orange and blue lines as a guide for robot angle in terms of the board
-
-#find blobs
-
-#make rectangle of the blob
-#distance=[(normal size of block)*(constant to regulate to mm or inches)]/(hight and width size of rectangle)
-#print(distance)
-
-#perfect track
-
-#line
-
-#curve
-
-#speed=distance, offset from perfect track
-#wheel angle when moving=color of block,robot angle,distance,speed
-sumError=0
-prevError=0
-sensor.reset()  # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.RGB565)  # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.QVGA)  # Set frame size to QVGA (320x240)
-sensor.set_vflip(True) # Flips the image vertically
-sensor.set_hmirror(True) # Mirrors the image horizontally
-sensor.skip_frames(time=2000)  # Wait for settings take effect.
-clock = time.clock()  # Create a clock object to track the FPS.
-def cal_hyp(adjacent,opposite):return math.sqrt(adjacent**2+opposite**2)
-def cal_adj(opposite,hypotenuse):return math.sqrt(hypotenuse**2-opposite**2)
-
-
-offsetSpeed=0
-cam_data=[0,0,0,0,0]
+st=SerialTalk()
+cam_data=[0,0,0]
 def cam():return cam_data# global cam_data
 st.add_command(cam, "repr")
 
-
-while True:
-    clock.tick()  # Update the FPS clock.
-    img=sensor.snapshot()  # Take a picture and return the image.
-    blobs=img.find_blobs([threshHold_red],area_threshold=20)
-    # print(len(blobs))
-    if len(blobs)>=1:
-        blob=blobs[0]
-        aveHW=(blob[2]+blob[3])/2   #average of the hight and width
-        aveBlock=(blockSizes[0][1]+blockSizes[0][2])/2
-        distance=(aveBlock*290)/aveHW #block aveHW is inversly proportional to distance
-        #print(" distance: "+str(distance))
-        #print(" aveHW: "+str(aveHW))
-        img.draw_circle(int(img.width()/2),int(img.height()/2),1,fill=True)
-        #x,y,w,h,cx,cy
-        img.draw_rectangle(blob[0:4],color=(0,255,0))
-        img.draw_cross(blob[5],blob[6],color=(0,255,0))
-        #imaginary rectangle
-        offsetA=(int(img.width()/2)-blob[5])*-1
-        #distance=10#dummy distance
-        if offsetA==0:offsetA=1
+sensor.reset()#initialize sensor
+sensor.set_pixformat(sensor.RGB565)#use color
+sensor.set_framesize(sensor.QVGA)#320x240 resolution
+sensor.set_vflip(True)#vertical flip to match world
+sensor.set_hmirror(True)#horizontal mirror to align view
+sensor.set_auto_whitebal(False)#disable auto white balance
+sensor.set_auto_gain(False)#disable auto gain
+sensor.skip_frames(time=2000)#wait for config to apply
+clock=time.clock()
+img_center=(160,120)
+def check_color_balance(img,img_debug,ROI_Y,ROI_H):
+    img_contrast=img.copy()
+    img_contrast.gamma_corr(gamma=1.3,contrast=2.2)
+    roi_left=(0,ROI_Y,img.width()//2,ROI_H)
+    roi_right=(img.width()//2,ROI_Y,img.width()//2,ROI_H)
+    BLACK_THRESH=(0, 93, -128, 0, -7, 36)
+    Lblack_blobs=img_contrast.find_blobs([BLACK_THRESH],roi=roi_left,pixels_threshold=200)
+    Rblack_blobs=img_contrast.find_blobs([BLACK_THRESH],roi=roi_right,pixels_threshold=200)
+    Lblack_pixels=sum([b.pixels() for b in Lblack_blobs])
+    Rblack_pixels=sum([b.pixels() for b in Rblack_blobs])
+    if True: #drawings for debugging
+        img_debug.draw_rectangle(roi_left,color=(255,0,0))#draw red box on left
+        img_debug.draw_rectangle(roi_right,color=(0,0,255))#draw blue box on right
+        for blob in Lblack_blobs:
+            img_debug.draw_rectangle(blob.rect(),color=(0,255,0))
+        for blob in Rblack_blobs:
+            img_debug.draw_rectangle(blob.rect(),color=(0,255,0))
+    black_balance=max(-1100,min(1100,(Lblack_pixels-Rblack_pixels)/8))
+    return int(black_balance)
+def target_point(img_debug,block,color,k):
+    offset_scaler=int(k*(block.y()+block.h()))
+    if color=="red":offset_x=(block.x()+block.w())+offset_scaler#right side-offset
+    if color=="green":offset_x=block.x()-offset_scaler#left side-offset
+    draw_x=max(0,min(319,offset_x))
+    img_debug.draw_circle(draw_x,block.cy(),3,color=(0,255,255),fill=True)#target for cursor
+    img_debug.draw_arrow(img_center[0],img_center[1],draw_x,img_center[1],color=(0,0,255))
+    error=offset_x-img_center[0]
+    img_debug.draw_string(img_center[0]-30,img_center[1]-10,"Err: "+str(error),color=(255,255,255),scale=1)
+    return error
+def find_block(img,img_debug):
+    img_contrast=img.copy()
+    img_contrast.gamma_corr(gamma=1.9, contrast=1.1,brightness=-0.1)
+    color="None"
+    blob=None
+    nearestRed=None
+    nearestGreen=None
+    red_val=0
+    green_val=0
+    #thresholds in LAB format for color filtering
+    threshold_red=(0, 40, 5, 127, -10, 55)
+    threshold_green=(25, 51, -85, -21, -7, 73)
+    #detect blobs with minimum size; no merging to prevent combining distant blocks
+    red=img_contrast.find_blobs([threshold_red],pixels_threshold=100,merge=False)
+    green=img_contrast.find_blobs([threshold_green],pixels_threshold=100,merge=False)
+    if not red and not green:return {"color":"None","blob":None}
+    if red:# evaluate all red blobs
+        for b in red:
+            img_debug.draw_rectangle(b.rect(),color=(255,0,0))
+            val=b.y()+b.h()
+            if val>red_val:# update nearest red
+                nearestRed=b
+                red_val=val
+    if green:# evaluate all green blobs
+        for b in green:
+            img_debug.draw_rectangle(b.rect(),color=(0,255,0))
+            val=b.y()+b.h()                            #(2*b.pixels())-b.area()
+            if val>green_val:# update nearest green
+                nearestGreen=b
+                green_val=val
+    # choose the blob with the higher score
+    if nearestRed and nearestGreen:
+        if red_val>=green_val:
+            color="red"
+            blob=nearestRed
+        else:
+            color="green"
+            blob=nearestGreen
+    elif nearestRed:
         color="red"
-        if color=="red":
-            img.draw_rectangle(blob[0]+blob[2],(blob[1]+blob[3])-robotSize[1],robotSize[0],robotSize[1],color=(0,255,0))
-            img.draw_cross(blob[0]+blob[2]+int(robotSize[0]/2),(blob[1]+blob[3])-int(robotSize[1]/2),color=(0,255,0))
-            offsetI=(int(img.width()/2)-(blob[0]+blob[2]+int(robotSize[0]/2)))
-            offsetA=offsetA*-1
-            botDirectionAngle=math.degrees(math.atan2(offsetA,distance))#relative to the block
-            botDirectionLength=cal_hyp(distance,offsetA)#relative to the block
-            #Atriangle=(distance,offsetA,botDirectionLength)a,o,h
-            #Itriangle=(botDirectionLength,abs(offsetI),cal_hyp(botDirectionLength,abs(offsetI)))a,o,h
-            if botDirectionAngle<0:botDirectionLength=(-abs(botDirectionLength))+(int(robotSize[0]/2)+(blob[2]/2))-6
-            turnAngle=math.degrees(math.atan2(offsetI,botDirectionLength))/2.5
-        if color=="green":
-            img.draw_rectangle(blob[0]-robotSize[0],(blob[1]+blob[3])-robotSize[1],robotSize[0],robotSize[1],color=(0,255,0))
-            img.draw_cross(blob[0]-int(robotSize[0]/2),(blob[1]+blob[3])-int(robotSize[1]/2),color=(0,255,0))
-            offsetI=(int(img.width()/2)-(blob[0]-int(robotSize[0]/2)))
-            botDirectionAngle=math.degrees(math.atan2(offsetA,distance))#relative to the block
-            botDirectionLength=cal_hyp(distance,offsetA)#relative to the block
-            #Atriangle=(distance,offsetA,botDirectionLength)a,o,h
-            #Itriangle=(botDirectionLength,abs(offsetI),cal_hyp(botDirectionLength,abs(offsetI)))a,o,h
-            if botDirectionAngle<0:botDirectionLength=(-abs(botDirectionLength))+int(robotSize[0]/2)+6
-            turnAngle=math.degrees(math.atan2(offsetI,botDirectionLength))/2.5
-        #print("offsetA: "+str(offsetA))
-        #print("offsetI: "+str(offsetI))
-        #print(botDirectionLength)
-        # print(turnAngle)
-        # if turnAngle<15 and turnAngle>-15:speed=100
-        # else:
-        #     if turnAngle<60 and turnAngle>-60:speed=75
-        #     else:speed=50
+        blob=nearestRed
+    elif nearestGreen:
+        color="green"
+        blob=nearestGreen
+    else:
+        color="None"
+        blob=None
+    return {"color":color,"blob":blob}#return blob info and color if found
+snap_distance=None
+peak_distance=0
+while True:
+    clock.tick()
+    #img_debug is for humans to interpret and contains drawings
+    img_debug=sensor.snapshot()#capture raw image
+    img_debug=img_debug.lens_corr(strength=1.53,x_corr=-0.04)#correct lens distortion
+    #img_debug.gamma_corr(gamma=1.9, contrast=1.1,brightness=-0.1)#debug
+    #img is for the robot to interpret and has no drawings for clean processing
+    img=img_debug.copy()#clean image
+    img_debug.draw_circle(img_center[0],img_center[1],1,color=(0,0,255))#cursor
+    white_balance=check_color_balance(img,img_debug,80,160)
+    block=find_block(img,img_debug)
+    distance=None
+    error=None
+    if block["color"]!="None":
+        # draw dot at blob center
+        img_debug.draw_circle(block["blob"].cx(),block["blob"].cy(),5,color=(0,0,255),fill=True)
+        distance=240-(block["blob"][1]+block["blob"][3])#works even when there is occlusion
+        snap_distance=distance
+        if distance>peak_distance:peak_distance=distance
+        error=target_point(img_debug,block["blob"],block["color"],0.85)
+    #print(time.time(),distance,snap_distance,peak_distance,error,white_balance)
 
-        #error=(img.width()/2)-(blob[0]+blob[2]+(robotSize[0]/2))
-
-        #interpret values to send to the robot
-        kp=1*21.42857142857143
-        ki=0
-        kd=0
-        speed=50
-        error=turnAngle-0
-        sumError+=error
-        compensation=(kp*error)+(ki*sumError)+(kd*(error-prevError))
-        prevError=error
-        speedPID=speed+abs(compensation)
-        print(speedPID)
-
-        compensation=(1*error)+(0*sumError)+(0*(error-prevError))
-        anglePID=-(turnAngle-(compensation))
-        print(anglePID)
+    cam_data=[white_balance,error,distance]
+    print(cam_data)
+    st.process()
 
 
 
-
-
-
-        # P=(1*error)#ratio of angle to gear rotation
-        # I=(0*sumError)
-        # D=(0*(error-prevError))
-        #compensation=P+I+D
-
-        #if turnAngle>30:offsetSpeed+=30
-        #if turnAngle<-30:offsetSpeed-=30
-        cam_data=[speedPID,anglePID,turnAngle,botDirectionAngle,distance]
-        print(cam_data)
-        st.process()
-        pyb.delay(50)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #print(clock.fps())  # Note: OpenMV Cam runs about half as fast when connected
-    # to the IDE. The FPS should increase once disconnected.
